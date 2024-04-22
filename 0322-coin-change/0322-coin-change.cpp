19b18feff59b510095e516a8ca2d4e1808752b21
Raj Goyal <raajj.goyal@gmail.com>
2024-04-22 16:39:31 +0530

    Time: 10 ms (98.96%), Space: 19.9 MB (41.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int f(vector<int>&dp,int amount,vector<int>&coins)
-    {
+    
+    int coinChange(vector<int>& coins, int amount) {
+        vector<long long>dp(amount+1,INT_MAX);
         if(amount==0) return 0;
         if(amount<0) return INT_MAX;
-        if(dp[amount]!=-1) return dp[amount];
-        int mini=INT_MAX;
-        int ans=INT_MAX;
+        
+        dp[0]=0;
         for(int i=0;i<coins.size();i++)
         {
-            mini=f(dp,amount-coins[i],coins);
-            if(mini!=INT_MAX) ans=min(mini+1,ans);
+            for(int j=coins[i];j<=amount;j++)
+            {
+                dp[j]=min(dp[j],dp[j-coins[i]]+1);
+                
+                
+            }
         }
-        return dp[amount]=ans;
-    }
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int ans=f(dp,amount,coins);
-        return ans==INT_MAX?-1:ans;
+        return dp[amount]==INT_MAX?-1:dp[amount];
+        
     }
 };
