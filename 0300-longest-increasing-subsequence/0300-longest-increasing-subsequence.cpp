19b3b2cbc46a4418acab06e6483050aee41585dd
Raj Goyal <raajj.goyal@gmail.com>
2024-04-23 13:17:16 +0530

    Time: 3 ms (98.25%), Space: 12.7 MB (93.53%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(vector<vector<int>>&dp,vector<int>&nums,int idx,int prev_idx,int n)
+    /*int f(vector<vector<int>>&dp,vector<int>&nums,int idx,int prev_idx,int n)
     {
         if(idx==n) return 0;
         if(dp[idx][prev_idx+1]!=-1) return dp[idx][prev_idx+1];
@@ -11,10 +11,27 @@ public:
          ans=max(ans,f(dp,nums,idx+1,prev_idx,n));
         return dp[idx][prev_idx+1]=ans;
 
-    }
+    }*/
     int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
+        // T.C. n**2,S.C. n**2
+        /*int n=nums.size();
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return f(dp,nums,0,-1,n);
+        return f(dp,nums,0,-1,n);*/
+
+        //T.C. nlog(n)
+        int n=nums.size();
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        for(int i=1;i<n;i++)
+        {
+            if(nums[i]>temp.back())
+            temp.push_back(nums[i]);
+            else
+            {
+                int idx=lower_bound(temp.begin(),temp.end(),nums[i])-temp.begin();
+                temp[idx]=nums[i];
+            }
+        }
+        return temp.size();
     }
 };
