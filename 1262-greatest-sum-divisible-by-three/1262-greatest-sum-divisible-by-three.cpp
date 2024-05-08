2c14476d837a373cc12258a0a8f701f18b5a433a
Raj Goyal <raajj.goyal@gmail.com>
2024-05-08 16:36:39 +0530

    Time: 46 ms (60.78%), Space: 40.7 MB (41.23%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maxSumDivThree(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n+1,vector<int>(3));
+        dp[0][1]=dp[0][2]=INT_MIN;
+    
+        for(int i=1;i<=n;i++)
+        {
+            if(nums[i-1]%3==0)
+            {
+                dp[i][0]= max(dp[i-1][0],dp[i-1][0]+nums[i-1]);
+                dp[i][1]= max(dp[i-1][1],dp[i-1][1]+nums[i-1]);
+                dp[i][2]= max(dp[i-1][2],dp[i-1][2]+nums[i-1]);
+            }
+            else if(nums[i-1]%3==1)
+            {
+                dp[i][0]= max(dp[i-1][0],dp[i-1][2]+nums[i-1]);
+                dp[i][1]= max(dp[i-1][1],dp[i-1][0]+nums[i-1]);
+                dp[i][2]= max(dp[i-1][2],dp[i-1][1]+nums[i-1]);
+            }
+            else if(nums[i-1]%3==2)
+            {
+                dp[i][0]= max(dp[i-1][0],dp[i-1][1]+nums[i-1]);
+                dp[i][1]= max(dp[i-1][1],dp[i-1][2]+nums[i-1]);
+                dp[i][2]= max(dp[i-1][2],dp[i-1][0]+nums[i-1]);
+            }
+        }
+        return dp[n][0];
+    }
+};
