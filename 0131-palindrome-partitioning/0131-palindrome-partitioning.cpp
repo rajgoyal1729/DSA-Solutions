009372a07b6227fe0f83a5ea82a40c477116151d
Raj Goyal <raajj.goyal@gmail.com>
2024-05-22 11:11:33 +0530

    Time: 297 ms (5.00%), Space: 176.5 MB (5.79%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isPal(string s, int start, int end)
+    {
+        if(start>end) return false;
+        while(start<=end)
+        {
+            if(s[start++]!=s[end--]) return false;
+        }
+        return true;
+    }
+    void solve(string s, vector<vector<string>>&ans, vector<string>path,int index)
+    {
+        if(index==s.length())
+        {
+            ans.push_back(path);
+            return ;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            if(isPal(s,index,i))
+            {
+                path.push_back(s.substr(index,i-index+1));
+                solve(s,ans,path,i+1);
+                path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>ans;
+        vector<string>path;
+        solve(s,ans,path,0);
+        return ans;
+    }
+};
